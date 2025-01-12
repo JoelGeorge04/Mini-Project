952dfbcf172969d2f439b2f8f8ee27a2959694ff
Joel George <129869710+JoelGeorge04@users.noreply.github.com>
2025-01-12 15:22:36 +0530

    -->

--- backend/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Express{
-    export interface Request {
-        user: import("../interface/auth.interface").UserData;              //now within the Express Requsest object, we have a 'user' property
-    }   
-}
